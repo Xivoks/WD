998bf3e022a9c131dff714bfc70312aed34403a1
Xivoks <33716560+Xivoks@users.noreply.github.com>
2020-03-05 14:43:19 +0100

    Add files via upload
    lesstion_2

---
+++ request.py
@@ -0,0 +1,64 @@
+import sys
+# task1
+# a = input("Enter some text")
+# print(a.count(" "))
+
+
+# task2
+# print("Enter two integer")
+# a=sys.stdin.readline() 
+# b=sys.stdin.readline() 
+# c=int (a)*int(b)
+# c=str(c)
+# c=sys.stdout.write(c)
+
+# task4
+# a=int(input("Enter value\n"))
+# print(abs(a))
+
+
+# task5
+#         # list=[1,2,3,4,5,6,7,8,9,10]
+# a=int(input("Enter value\n"))
+# b=int(input("Enter value\n"))
+# c=int(input("Enter value\n"))
+#         # if a in list and a>b and b>c:
+# if 0<a and a<=10 and a>b and b>c:
+#     print("condition is met")
+# else:
+#     print("nope")
+
+# task6
+# for i in range (5,100,5):
+#     print(i)
+
+# task7
+# for i in range(5):
+#     a=int(input("Enter value\n"))
+#     a=a**a
+#     print(a)
+
+# task8
+# a=int(input("How many values u want add to list? \n"))
+# i=0
+# list=[]
+# while i<a:
+#     b=int(input("Enter value: \n"))
+#     list.insert(i,b)
+#     i+=1
+# print (list)
+
+# task9
+# a=int(input("Enter value \n"))
+# suma=0
+# for i in str(a):
+#     suma=suma+int(i)
+# print (suma)
+
+
+# task10
+a=int(input("Enter value \n"))
+while a>10:
+    a=int(input("Enter value \n"))
+for i in range (1,a+1,1):
+    print("A"*i)

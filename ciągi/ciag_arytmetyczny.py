7ac341c03861b3bd813e789a7109b9fd4df810f0
Xivoks <33716560+Xivoks@users.noreply.github.com>
2020-04-02 21:00:19 +0200

    poprawiona_wersja

--- ciągi/ciag_arytmetyczny.py
+++ ciągi/ciag_arytmetyczny.py
@@ -4,9 +4,5 @@ def wzor(a1=1, r=1, ile=1):
 
 
 def suma(a1=1, r=1, ile=10):
-    suma_ciagu = (((2*a1)+((ile-1)*r))*0.5)*ile
+    suma_ciagu = (((2*a1)+((ile-1)*r))/2.0)*ile
     return(suma_ciagu)
-
-
-ref = suma
-ref(1, 2, 4)
--- ciągi/ciag_geometryczny.py
+++ ciągi/ciag_geometryczny.py
@@ -1,10 +1,10 @@
 def wzor(a1=1, q=1, ile=1):
     wyraz = a1*q**ile
-    print(wyraz)
+    return(wyraz)
 
 
 def suma(a1=1, q=1, ile=10):
     suma_ciagu = a1
     for x in range(1, ile):
         suma_ciagu += a1*(q**x)
-    print(suma_ciagu)
+    return(suma_ciagu)

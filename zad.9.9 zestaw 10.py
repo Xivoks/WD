a1cfee1d881e345c85ecf0d24b65c92313fb5ea8
Xivoks <33716560+Xivoks@users.noreply.github.com>
2020-05-26 20:51:24 +0200

    Add files via upload

--- zad.9.9 zestaw 10.py
+++ zad.9.9 zestaw 10.py
@@ -4,8 +4,8 @@ import pandas as pd
 df = pd.read_csv('pandas/zamowienia.csv', header=0, delimiter=";")
 df_np = np.array(df)
 dane = df.groupby(['Sprzedawca']).agg({"idZamowienia": ["count"]})
-
-wedges, texts, autotexts = plt.pie(dane.values, labels=dane.index.values,
+Explode = [1/(i-0.1) for i in range(len(dane.index.values))]
+wedges, texts, autotexts = plt.pie(dane.values, explode=Explode, labels=dane.index.values,
                                    autopct=lambda pct: "{:.1f}%".format(pct), textprops=dict(color="black"))
 plt.setp(autotexts, size=14, weight="bold")
 plt.title("Pierwsza wersja wykresu")

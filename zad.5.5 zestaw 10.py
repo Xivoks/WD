bd26bb614a75e993171ea6689e948f5715a51c31
Xivoks <33716560+Xivoks@users.noreply.github.com>
2020-05-26 20:53:17 +0200

    Delete zad.5.5 zestaw 10.py

--- zad.5.5 zestaw 10.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from sklearn import datasets
-
-iris = datasets.load_iris()
-plt.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target, s=5)
-plt.show()
